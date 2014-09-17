9d8de2b70726ab5fe6606c26ba72067bbbf62004
yyankova <yana.o.yankova@gmail.com>
2014-09-17 13:49:25 +0300

    Controller for Offers

--- BioMarket.Web/Models/OfferModel.cs
+++ BioMarket.Web/Models/OfferModel.cs
@@ -3,9 +3,47 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Web;
+
+    using BioMarket.Models;
 
     public class OfferModel
     {
+        public static Expression<Func<Offer, OfferModel>> FromOffer
+        {
+            get
+            {
+                return o => new OfferModel
+                {
+                    Id = o.Id,
+                    ProductId = o.ProductId,
+                    Quantity = o.Quantity,
+                    ProductPhoto = o.ProductPhoto,
+                    BoughtBy = o.BoughtBy,
+                    BoughtById = o.BoughtById,
+                    PostDate = o.PostDate,
+                    BoughtDate = o.BoughtDate
+                };
+            }
+        }
+
+        public int Id { get; set; }
+
+        public virtual ProductModel Product { get; set; }
+
+        public int ProductId { get; set; }
+
+        public double Quantity { get; set; }
+
+        public string ProductPhoto { get; set; }
+
+        public virtual Account BoughtBy { get; set; }
+
+        public int BoughtById { get; set; }
+
+        public DateTime PostDate { get; set; }
+
+        public DateTime BoughtDate { get; set; }
     }
 }
--- BioMarket.Web/Models/ProductModel.cs
+++ BioMarket.Web/Models/ProductModel.cs
@@ -4,8 +4,62 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Web;
+    using System.Linq.Expressions;
+
+    using BioMarket.Models;
 
     public class ProductModel
     {
+        public static Expression<Func<Product, ProductModel>> FromFarm
+        {
+            get
+            {
+                return a => new ProductModel
+                {
+                    Id = a.Id,
+                    Name = a.Name,
+                    Price = a.Price,
+                    Deleted = a.Deleted,
+                    FarmId = a.FarmId
+                };
+            }
+        }
+
+        public static Expression<Func<Product, ProductModel>> FromFarmWithOffers
+        {
+            get
+            {
+                return a => new ProductModel
+                {
+                    Id = a.Id,
+                    Name = a.Name,
+                    Price = a.Price,
+                    Deleted = a.Deleted,
+                    FarmId = a.FarmId,
+                    Offers = a.Offers.Select(o => new OfferModel()
+                    {
+                        BoughtBy = o.BoughtBy,
+                        BoughtDate = o.BoughtDate
+                    })
+                };
+            }
+        }
+
+
+        private ICollection<Offer> offers;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public virtual Account Farm { get; set; }
+
+        public int FarmId { get; set; }
+
+        public virtual IEnumerable<OfferModel> Offers { get; set; }
     }
 }
